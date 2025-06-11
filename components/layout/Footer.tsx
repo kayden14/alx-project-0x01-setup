f678712321e0fab23ddf82727289541b138bc5c3
kayden14 <faithtega14@gmail.com>
2025-06-11 02:58:05 -0700

    Initial setup for ALX Project 0x01

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,7 @@
+const Footer: React.FC = () => (
+  <footer className="bg-gray-800 text-white p-4 text-center">
+    <p>© 2025 ALX</p>
+  </footer>
+);
+
+export default Footer;
---
+++ pages/users/index.tsx
@@ -0,0 +1,7 @@
+const UsersPage: React.FC = () => (
+  <div>
+    <h1 className="text-3xl">Users Page</h1>
+  </div>
+);
+
+export default UsersPage;
